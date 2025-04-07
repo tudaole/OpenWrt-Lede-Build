f86a77bb963bc8bdce96abe4edece2a0146c67f4
tudaole <122336705+tudaole@users.noreply.github.com>
2025-04-07 11:23:24 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-#=================================================
-# DaoDao's script
-#=================================================
 ##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
-               
-sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
-
+#sed -i "1isrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk\n" feeds.conf.default
+echo -e "\nsrc-git OpenWrt-Packages https://github.com/tudaole/OpenWrt-ImmortalWrt-Packages" >> feeds.conf.default
