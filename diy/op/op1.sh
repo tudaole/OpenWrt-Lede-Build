7e15848fef857c5522df15ab53be499f23014722
tudaole <122336705+tudaole@users.noreply.github.com>
2025-04-07 11:23:30 +0800

    Update op1.sh

--- diy/op/op1.sh
+++ diy/op/op1.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-#=================================================
-# DaoDao's script
-#=================================================
 ##添加自己的插件库
 #echo -e "\nsrc-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk" >> feeds.conf.default
 echo -e "\nsrc-git OpenWrt-Packages https://github.com/tudaole/OpenWrt-ImmortalWrt-Packages" >> feeds.conf.default
